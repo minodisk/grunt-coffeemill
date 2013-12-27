3afc704ed95bf805c3055e2ad4bbec4ef6e304b6
minodisk <daisuke.mino@gmail.com>
2013-12-27 14:29:01 +0900

    Respond to event-driven messaging system

--- src/grunt-coffeemill.coffee
+++ src/grunt-coffeemill.coffee
@@ -1,5 +1,22 @@
-{ CoffeeMill } = require 'coffeemill'
+#{ CoffeeMill } = require 'coffeemill'
+{ CoffeeMill } = require '/Users/minodisk/workspace/coffeemill/lib/coffeemill'
 
 module.exports = (grunt) ->
-  grunt.registerMultiTask 'coffeemill', 'coffemill task', ->
-    new CoffeeMill(grunt.config('coffeemill')[@target]).run()
+  grunt.registerMultiTask 'coffeemill', ->
+    done = @async()
+    warnCount = 0
+    errorCount = 0
+
+    new CoffeeMill(grunt.config('coffeemill')[@target].options)
+    .run()
+    .on 'warn', (message) ->
+        warnCount++
+        grunt.fail.warn message
+    .on 'error', (message) ->
+        errorCount++
+        grunt.fail.fatal message
+    .on 'created', (filepath) ->
+        grunt.log.writeln "File #{filepath.cyan} created"
+    .on 'complete', (filenum) ->
+        grunt.log.writeln "Done, with #{warnCount} warns and #{errorCount} errors.".green
+        done()
--- tasks/grunt-coffeemill.js
+++ tasks/grunt-coffeemill.js
@@ -1,9 +1,24 @@
 var CoffeeMill;
 
-CoffeeMill = require('coffeemill').CoffeeMill;
+CoffeeMill = require('/Users/minodisk/workspace/coffeemill/lib/coffeemill').CoffeeMill;
 
 module.exports = function(grunt) {
-  return grunt.registerMultiTask('coffeemill', 'coffemill task', function() {
-    return new CoffeeMill(grunt.config('coffeemill')[this.target]).run();
+  return grunt.registerMultiTask('coffeemill', function() {
+    var done, errorCount, warnCount;
+    done = this.async();
+    warnCount = 0;
+    errorCount = 0;
+    return new CoffeeMill(grunt.config('coffeemill')[this.target].options).run().on('warn', function(message) {
+      warnCount++;
+      return grunt.fail.warn(message);
+    }).on('error', function(message) {
+      errorCount++;
+      return grunt.fail.fatal(message);
+    }).on('created', function(filepath) {
+      return grunt.log.writeln("File " + filepath.cyan + " created");
+    }).on('complete', function(filenum) {
+      grunt.log.writeln(("Done, with " + warnCount + " warns and " + errorCount + " errors.").green);
+      return done();
+    });
   });
 };
